87e8720b2e29f57dba5069c4859a3166c46cc7f8
Fernando Pedraza <fpedraza@solvd.com>
2026-04-07 18:00:00 -0500

    Add pre-commit configuration

--- locals.tf
+++ locals.tf
@@ -1,6 +1,5 @@
 # Remove this file after the s3 bucket for remote state is created
 
-locals {
-  s3_terraform_state_key = "${var.org}-${var.name_prefix}-${var.env_name}-remote-state-backend.tfstate"
-}
-
+# locals {
+#   s3_terraform_state_key = "${var.org}-${var.name_prefix}-${var.env_name}-remote-state-backend.tfstate"
+# }
