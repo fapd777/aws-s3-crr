bdba1576f3febb246e0298483d33e5e1b5f556e7
Fernando Pedraza <fpedraza@solvd.com>
2026-04-29 16:07:17 -0500

    tf-module-version

--- s3-bucket-tfstate.tf
+++ s3-bucket-tfstate.tf
@@ -3,7 +3,7 @@
 ################################################################################
 
 module "terraform_state" {
-  source        = "git::https://github.com/fapd777/terraform-module-state-s3-bucket.git"
+  source        = "git::https://github.com/fapd777/terraform-module-state-s3-bucket.git?ref=v1.0.0"
   name_prefix   = var.name_prefix
   name_suffix   = var.region
   log_bucket_id = var.logging_bucket
